4b280919e1a89466c99f700920b149cf4207dd8e
amineohn <amineprojet7@gmail.com>
2021-10-28 11:32:49 +0200

    feat(theme): theme, add colors tailwind fix dom new design page.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import ReactDOM from "react-dom";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  if (typeof window !== "undefined") {
+    const container = document.getElementById("__next");
+    ReactDOM.hydrate(<Component />, container);
+  }
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
+export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,39 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        black: colors.black,
+        white: colors.white,
+        gray: colors.gray,
+        indigo: colors.indigo,
+        red: colors.red,
+        yellow: colors.yellow,
+        amber: colors.amber,
+        lime: colors.lime,
+        green: colors.green,
+        emerald: colors.emerald,
+        teal: colors.teal,
+        sky: colors.sky,
+        blue: colors.blue,
+        violet: colors.violet,
+        fuchsia: colors.fuchsia,
+        rose: colors.rose,
+        cyan: colors.cyan,
+        orange: colors.orange,
+        warmGray: colors.warmGray,
+        trueGray: colors.trueGray,
+        coolGray: colors.coolGray,
+        blueGray: colors.blueGray,
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
