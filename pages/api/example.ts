7ee3ffc61fe1793c25de4be91d466283e6502974
amineohn <amineprojet7@gmail.com>
2021-10-28 14:41:37 +0200

    missing import type.

--- pages/api/example.ts
+++ pages/api/example.ts
@@ -1,13 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
-
-type Data = {
-  name: string;
-};
+import { Example } from "../../libs/types";
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Example>,
   han: NextApiHandler<NextApiHandler>
 ) {
   res.status(200).json({ name: "hello" });
