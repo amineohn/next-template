8d906ec87377bfe8906271c2c2b4a84cd91a47bf
amineohn <amineprojet7@gmail.com>
2021-10-28 16:19:30 +0200

    use this solution

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,4 +1,4 @@
-import ToggleTheme from "./ToggleTheme";
+import { useTheme } from "next-themes";
 import { useRouter } from "next/router";
 const Navigation = () => {
   const router = useRouter();
@@ -14,6 +14,37 @@ const Navigation = () => {
         </a>
       </div>
     </div>
+  );
+};
+
+const ToggleTheme = () => {
+  const { theme, setTheme } = useTheme();
+
+  const getNextTheme = (): string => {
+    if (theme === "dark") return "light";
+    if (theme === "light") return "system";
+    return "dark";
+  };
+
+  return (
+    <button
+      type="button"
+      className="w-10 h-10"
+      onClick={() => setTheme(getNextTheme())}
+    >
+      {(() => {
+        switch (theme) {
+          case "dark":
+            return <span className="text-xl">&#127762;</span>;
+          case "system":
+            return <span className="text-xl">&#127763;</span>;
+          case "light":
+            return <span className="text-xl">&#127765;</span>;
+          default:
+            return "system";
+        }
+      })()}
+    </button>
   );
 };
 
