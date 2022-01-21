68d2ed6d2a91943ae694b0d14ca72b4d3f302197
isveny <amineprojet7@gmail.com>
2022-01-21 14:53:20 +0100

    big update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,7 @@
 import "../styles/globals.css";
 import React from "react";
-import { ThemeProvider } from "next-themes";
 import { NextPage } from "next";
 import { NextSeo } from "next-seo";
-
-import dynamic from "next/dynamic";
-const Navigation = dynamic(() => import("../components/Navigation"), {
-  ssr: false,
-});
 import { configuration } from "../util/configuration";
 export default function MyApp({
   Component,
@@ -22,10 +16,7 @@ export default function MyApp({
         title={configuration.title}
         description={configuration.description}
       />
-      <ThemeProvider defaultTheme="dark" attribute="class">
-        <Navigation />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </>
   );
 }
